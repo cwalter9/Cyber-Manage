7a56ea81597d84483cfd1115cded70de10e42b93
Chad Walter <cwalter9@gmail.com>
2011-07-09 20:19:13 -0400

    Added Sensatronic EM1 snmp checks

--- sql/install.sql
+++ sql/install.sql
@@ -8,3 +8,10 @@ CREATE TABLE `swVersions` (
   `version` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
+
+CREATE TABLE `sensatronic_em1` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `sensor` varchar(128) DEFAULT NULL,
+  `value` float(5,1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1
